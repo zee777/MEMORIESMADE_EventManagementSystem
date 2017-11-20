626bc710a86d223653bf37a57a2397d5fe5b2143
Karen Shane Adorico <karenshanekaren@gmail.com>
2017-11-19 18:11:28 -0800

    added new php file for the user profile

---
+++ public/profile.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title> MemoriesMade </title>
+			<link href="../styles/profile_styles.css" type="text/css" rel="stylesheet"/>
+		</head>
+		<body>
+			<div id="header">
+				<div id="header_cont">
+						<div id="header_logo"><a href="home.php"><img src="..\images\logo_here.png" alt="logo here please"></a></div>
+						<div class="dropdown">
+							<button class="dropbtn">USER</button>
+							<div class="dropdown-content">
+								<a href="profile.php">View Profile </a>
+								<a href="index.php">Log out</a>
+							</div>
+						</div>
+				</div>
+			</div>
+				
+			<div id="backgroundcolor">
+				<div id="bodycont">
+					<p id="stylep1">User Name</p>
+					<p id="editprof"><a href="#">EDIT PROFILE</a></p>
+				</div>
+			</div>
+			
+				<div id="centernav">
+					<ul>
+						<li>
+							<div class="dropdown_centernav">
+							<button class="dropbtn_centernav">UPCOMING EVENTS</button>
+							<div class="dropdown-content_centernav">
+							<h2> No Upcoming events. </h2>
+							</div>
+							</div>
+						</li>
+						<li>
+							<div class="dropdown_centernav">
+							<button class="dropbtn_centernav">SAVED EVENTS</button>
+							<div class="dropdown-content_centernav">
+									<thead class="table_saved">
+									<p>
+									<tr>
+										<th>Event Title &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+										<th>Date Start &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>           
+										<th>Time</th>
+										
+									</tr>
+									</p>
+								</thead>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			
+			<div id="footer">
+				<div class="footer_con">
+					<p class="stylef">About us</p><hr>
+					<ul class="stylep_con">
+						<li><a href="#">What is MemoriesMade?</a></li>
+						<li><a href="#">Developers</a></li>
+					</ul>
+				</div>
+				<div class="footer_con">
+					<p class="stylef">Use MemoriesMade</p><hr>
+					<ul class="stylep_con">
+						<li><a href="#">How it works?</a></li>
+						<li><a href="#">MemoriesMade User Reviews</a></li>
+					</ul>
+				</div>
+				<div class="footer_con">
+					<p class="stylef">Contact Us</p><hr>
+					<ul class="stylep_con">
+						<li><a href="#">282-1267</a></li>
+						<li><a href="#">E-mail</a></li>
+						<li><a href="#">Facebook</a></li>
+						<li><a href="#">Instagram</li>
+						<li><a href="#">Twitter</a></li>
+					</ul>
+				</div>
+			</div>
+			
+			<div id="copyright">
+				<center><p>&#x24B8;MemoriesMade 2017</p></center>
+			</div>
+		</body>
+	</html>
